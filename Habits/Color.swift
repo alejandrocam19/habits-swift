f7cfe5fb3234f89888c119608439516ff2e5504c
alejandrocam19 <alejandrocampagna19@gmail.com>
2021-07-30 16:19:40 +0300

    Created base networking code

---
+++ Habits/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  Habits
+//
+//  Created by Олег Бабыр on 30.07.2021.
+//
+
+import Foundation
+
+struct Color {
+    let hue: Double
+    let saturation: Double
+    let brightness: Double
+}
---
+++ Habits/Habit.swift
@@ -0,0 +1,20 @@
+//
+//  Habit.swift
+//  Habits
+//
+//  Created by Олег Бабыр on 30.07.2021.
+//
+
+import Foundation
+
+struct Habit {
+    let name: String
+    let category: Category
+    let info: String
+}
+
+struct Category {
+    let name: String
+    let color: Color
+}
+
